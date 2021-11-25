5bbbafe8e62bd02450264cd2c096590d9e5121eb
Ashutosh Kumar <63195712+ashutosh-hep@users.noreply.github.com>
2021-11-25 23:43:52 +0530

    Update Face_Mask_Detection.py

--- Face_Mask_Detection.py
+++ Face_Mask_Detection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[25]:
+# In[8]:
 
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -28,155 +28,167 @@ import os
 import pandas as pd
 from keras.preprocessing import image
 from keras.models import load_model
-from keras.applications.vgg16 import preprocess_input
+import warnings
+warnings.filterwarnings('ignore')
 
 
-# In[26]:
+# In[2]:
 
 
-INIT_LR = 1e-4
-EPOCHS = 10
-BS = 32
-dataset = "M/dataset"
 
 
-# In[27]:
+
+# In[ ]:
 
 
-args={}
-args["dataset"]=dataset
 
 
-# In[28]:
+
+# In[9]:
 
 
+#visualization
 images = ['With_Mask','Without_Mask'] 
 data = [1915,1918] 
 plt.pie(data, labels = images)
 plt.show()
 
 
-# In[29]:
+# In[10]:
+
+
+import matplotlib.pyplot as plt
 
 
 x = ['With_Mask','Without_Mask']
 y = [1915,1918] 
 plt.barh(x, y)
 
+
 for index, value in enumerate(y):
     plt.text(value, index, str(value))
 
 
-# In[30]:
+# In[11]:
 
 
-imagePaths = list(paths.list_images(args["dataset"]))
+#Data preprocessing
+
+print("    Progress --- Loading Images")
+
+DIRECTORY = r"M\dataset"
+CATEGORIES = ["with_mask", "without_mask"]
+
 data = []
 labels = []
-# loop over the image paths
-for imagePath in imagePaths:
-	# extracting the class label from the filename
-	label = imagePath.split(os.path.sep)[-2]
-	# loading the input image (224x224) and preprocess it
-	image = load_img(imagePath, target_size=(224, 224))
-	image = img_to_array(image)
-	image = preprocess_input(image)
-	# updating the data and labels lists, respectively
-	data.append(image)
-	labels.append(label)
-# converting the data and labels to NumPy arrays
-data = np.array(data, dtype="float32")
-labels = np.array(labels)
+
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)  
+    for img in os.listdir(path):
+        img_path = os.path.join(path, img)  
+        image = load_img(img_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = preprocess_input(image)
+
+        data.append(image)
+        labels.append(category)
+        
 
 
-# In[31]:
+# In[12]:
 
 
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
-# partitioning the data into training and testing splits using 80% of
-# the data for training and the remaining 20% for testing
+
+        
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
 (X_train, X_test, Y_train, Y_test) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
-# constructing the training image generator for data augmentation
+    test_size=0.20, stratify=labels, random_state=42)
+
 aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest")
 
 
-# In[32]:
+# In[ ]:
 
 
-labels
 
 
-# In[33]:
+
+# In[13]:
 
 
+#model Creation
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
-# constructing the head of the model that will be placed on top of the
-# the base model
+    input_tensor=Input(shape=(224, 224, 3)))
+
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
 headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
-# placing the head FC model on top of the base model (this will become
-# the actual model we will train)
+
+
 model = Model(inputs=baseModel.input, outputs=headModel)
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
+
 for layer in baseModel.layers:
-	layer.trainable = False
+    layer.trainable = False
 
 
-# In[34]:
+# In[ ]:
 
 
-X_train.shape,X_test.shape,Y_train.shape,Y_test.shape
 
 
-# In[35]:
+
+# In[14]:
 
 
-print("[INFO] compiling model...")
+#configuring the model.
+print("    Progress --- configuring model")
+
+INIT_LR = 1e-4
+EPOCHS = 10
+BS = 32
+
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
-# training the head of the network
-print("[INFO] training head...")
+    metrics=["accuracy"])
+
+#training the model
+print("    Progress --- training model")
 H = model.fit(
-	aug.flow(X_train, Y_train, batch_size=BS),
-	steps_per_epoch=len(X_train) // BS,
-	validation_data=(X_test, Y_test),
-	validation_steps=len(X_test) // BS,
-	epochs=EPOCHS)
+    aug.flow(X_train, Y_train, batch_size=BS),
+    steps_per_epoch=len(X_train) // BS,
+    validation_data=(X_test, Y_test),
+    validation_steps=len(X_test) // BS,
+    epochs=EPOCHS)
 
 
-# In[44]:
+# In[15]:
 
 
+#testing the model
 predIdxs = model.predict(X_test, batch_size=BS)
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
+
 predIdxs = np.argmax(predIdxs, axis=1)
-# showing a nicely formatted classification report
+
 print(classification_report(Y_test.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
-# serializeing the model to disk
-print("[INFO] saving mask detector model...")
-model.save('model11.h5')
+    target_names=lb.classes_))
 
 
-# In[45]:
+# In[16]:
 
 
 from sklearn.metrics import confusion_matrix
@@ -185,6 +197,7 @@ total = sum(sum(cm))
 acc = (cm[0, 0] + cm[1, 1]) / total
 sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
 specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
+
 # showing the confusion matrix, accuracy, sensitivity, and specificity
 print(cm)
 print("acc: {:.4f}".format(acc))
@@ -192,9 +205,10 @@ print("sensitivity: {:.4f}".format(sensitivity))
 print("specificity: {:.4f}".format(specificity))
 
 
-# In[46]:
+# In[17]:
 
 
+#perfomance visualization
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
@@ -208,12 +222,11 @@ plt.ylabel("Loss/Accuracy")
 plt.legend(loc="lower left")
 
 
-# In[47]:
+# In[19]:
 
 
+#prediction of images 
 from keras.preprocessing import image
-from keras.models import load_model
-from keras.applications.vgg16 import preprocess_input
 img = image.load_img('M/dataset/without_mask/0_0_huangtingting_0008.jpg', target_size=(224, 224))
 imgplot = plt.imshow(img)
 x = image.img_to_array(img)
@@ -227,7 +240,7 @@ else:
   print('Prediction: MASK')
 
 
-# In[48]:
+# In[20]:
 
 
 img = image.load_img('M/dataset/with_mask/0_0_0.jpg', target_size=(224, 224))
@@ -243,7 +256,7 @@ else:
   print('    Prediction: MASK')
 
 
-# In[49]:
+# In[21]:
 
 
 img = image.load_img('M/dataset/with_mask/0_0_0 copy 15.jpg', target_size=(224, 224))
@@ -259,7 +272,7 @@ else:
   print('    Prediction: MASK')
 
 
-# In[50]:
+# In[22]:
 
 
 img = image.load_img('M/dataset/without_mask/0_0_caizhuoyan_0027.jpg', target_size=(224, 224))
@@ -275,7 +288,7 @@ else:
   print('    Prediction: MASK')
 
 
-# In[53]:
+# In[23]:
 
 
 img = image.load_img('M/dataset/without_mask/0_0_chenhaomin_0117.jpg', target_size=(224, 224))
